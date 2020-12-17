c0590f219582b78b68f1d85ff49fecb7553a10e1
suichen <1456587116@qq.com>
2020-12-17 22:41:58 +0800

    leetcode 70

---
+++ src/main/java/leetcode/L070.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class L070 {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 2;
+        }
+
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        L070 l070 = new L070();
+        int num = l070.climbStairs(8);
+        System.out.println(num);
+    }
+}
